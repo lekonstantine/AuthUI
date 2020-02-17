7fa90e81cfa295276ebfee56581796cf558f76c8
Konstantin Skoblik <konstantin.skoblik@gmail.com>
2020-02-17 14:25:42 +0800

    first commit

---
+++ AuthUI/AuthUI.h
@@ -0,0 +1,19 @@
+//
+//  AuthUI.h
+//  AuthUI
+//
+//  Created by admin on 2/17/20.
+//  Copyright © 2020 Fleet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthUI.
+FOUNDATION_EXPORT double AuthUIVersionNumber;
+
+//! Project version string for AuthUI.
+FOUNDATION_EXPORT const unsigned char AuthUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthUI/PublicHeader.h>
+
+
---
+++ AuthUIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthUIExample
+//
+//  Created by admin on 2/17/20.
+//  Copyright © 2020 Fleet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
